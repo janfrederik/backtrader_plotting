c177c6b8d16d07a7880f5da51243aa231efe5741
Jan Frederik Léger <janfrederik@gmail.com>
2025-01-13 10:55:04 +0000

    Keep the alpha specified in the color

--- backtrader_plotting/bokeh/utils.py
+++ backtrader_plotting/bokeh/utils.py
@@ -8,13 +8,7 @@ from bokeh.models import ColumnDataSource
 
 
 def convert_color(color):
-    """if color is a float value then it is interpreted as a shade of grey and converted to the corresponding html color code"""
-    try:
-        val = round(float(color) * 255.0)
-        hex_string = '#{0:02x}{0:02x}{0:02x}'.format(val)
-        return hex_string
-    except ValueError:
-        return matplotlib.colors.to_hex(color)
+    return matplotlib.colors.to_hex(color, keep_alpha=True)
 
 
 def sanitize_source_name(name: str) -> str:
